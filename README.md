d4ffeeaa3807d8d39702495a8ddde5bc6217692c
noahtq <vfx.noaht@gmail.com>
2024-08-02 07:17:25 -0500

    Adding and linking quest_image_seq library

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Quist Keyer
+# Quest Keyer
 A better chroma keyer for commercial VFX workflows
 
 ## Versions (Current 0.0.0)
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include "../include/quest_seq_lib.h"
 
 using namespace std;
 
 int main()
 {
+    Quest::ImageSeq seq;
+    seq.open("../media/test_media/videos/image_sequences/small_dog_001/small_dog_001_%04d.png");
+    for (cv::Mat frame : seq) {
+        cv::GaussianBlur(frame, frame, cv::Size(55, 55), 0, 0, cv::BORDER_CONSTANT);
+    }
+    seq.render("../media/test_media/videos/image_sequences/small_dog_001_rendered/small_dog_001_rendered_%04d.png");
 
     return 0;
 }
