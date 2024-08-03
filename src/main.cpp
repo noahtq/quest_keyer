efac514acb77973e339e6d1caa0797768f62d62f
noahtq <vfx.noaht@gmail.com>
2024-08-02 20:25:28 -0500

    Remove wxwidgets

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,10 @@
 
 using namespace std;
 
-int main()
-{
+int main() {
     Quest::Keyer keyer;
     keyer.image_seq.open("../media/test_media/videos/image_sequences/small_dog_001/small_dog_001_%04d.png");
-    keyer.chromaKey(cv::Scalar(133, 160, 69));
+    keyer.chromaKey(cv::Scalar(133, 160, 69), 5);
     keyer.image_seq.render("../media/test_media/videos/image_sequences/small_dog_001_rendered/small_dog_001_rendered_%04d.png");
 
     return 0;
