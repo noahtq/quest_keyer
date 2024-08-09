eca6b1649cb103ce12caabdd7b653c456186c62f
noahtq <vfx.noaht@gmail.com>
2024-08-09 07:21:34 -0500

    Add functionality to play button

--- quest_keyer_ui/renderer.js
+++ quest_keyer_ui/renderer.js
@@ -1,3 +1,5 @@
+const playbackFPS = 30
+
 // IO elements
 const inputSearchBtn = document.getElementById('search-input-btn')
 const filePathElement = document.getElementById('input-path')
@@ -73,14 +75,18 @@ prevFrameBtn.addEventListener('click', () => {
     }
 })
 
+function getNextFrame() {
+    let updateData = {
+        originalProxyPath: viewerState.originalProxyPath,
+        keyedProxyPath: viewerState.keyedProxyPath,
+        currentFrame: viewerState.currentFrame + 1
+    }
+    viewerUpdate(updateData)
+}
+
 nextFrameBtn.addEventListener('click', () => {
     if (viewerState.frameLength > 0 && viewerState.currentFrame < viewerState.frameLength) {
-        let updateData = {
-            originalProxyPath: viewerState.originalProxyPath,
-            keyedProxyPath: viewerState.keyedProxyPath,
-            currentFrame: viewerState.currentFrame + 1
-        }
-        viewerUpdate(updateData)
+        getNextFrame()
     }
 })
 
@@ -103,6 +109,24 @@ endBtn.addEventListener('click', () => {
             currentFrame: viewerState.frameLength
         }
         viewerUpdate(updateData)
+    }
+})
+
+let currentlyPlaying = false
+const delay = ms => new Promise(res => setTimeout(res, ms));
+playBtn.addEventListener('click', async () => {
+    if (viewerState.frameLength > 0) {
+        if (!currentlyPlaying) {
+            currentlyPlaying = true
+            while (viewerState.currentFrame < viewerState.frameLength && currentlyPlaying) {
+                getNextFrame()
+                let milleDelay = 1000 / playbackFPS
+                await delay(milleDelay)
+            }
+            currentlyPlaying = false
+        } else {
+            currentlyPlaying = false
+        }
     }
 })
 
