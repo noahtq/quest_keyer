9c5324b6e5d03c5b60569f9d7c2d253234a91814
noahtq <vfx.noaht@gmail.com>
2024-09-02 21:30:51 -0500

    Connecting frontend and backend as one executable by having CPP backend start frontend

--- quest_keyer_backend/main.cc
+++ quest_keyer_backend/main.cc
@@ -1,5 +1,14 @@
 #include <drogon/drogon.h>
+#include <unistd.h>
+
 int main() {
+
+    std::stringstream ss;
+    ss << "open ";
+    std::filesystem::path frontend_path = "../../quest_keyer_ui/out/quest_keyer_ui-darwin-arm64/quest_keyer_ui.app";
+    ss << std::filesystem::canonical(frontend_path);
+    system((ss.str()).c_str());
+
     //Set HTTP listener address and port
     drogon::app().addListener("0.0.0.0", 5555);
     //Load config file
